bd70a82f689e1ad4a9d93d96e2c73f18e1ee11cb
Max Graham <m.huw.graham@gmail.com>
2019-01-09 15:58:22 +0000

    Added and cleared up comments,

--- character-positioning.js
+++ character-positioning.js
@@ -1,3 +1,15 @@
+/************************************************
+Character Positioning Assignment
+
+Goal: Create a function that takes in a string and return the locations
+of each unique character.
+
+ToDo:
+*Very simular to previous assignment, base off of it
+*Instead of adding to a letter counter, create an array for each new
+  letter, and push new indexes as they appear.
+**************************************************/
+
 console.log(countLetters("This is a test"));
 
 function countLetters(input){
